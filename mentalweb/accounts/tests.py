07285a50d3832a200ff1b72bd867827bf08d0e58
namukuru <namukurulauryn@gmail.com>
2025-07-02 17:36:47 +0300

    User Authentication and tests

--- mentalweb/accounts/tests.py
+++ mentalweb/accounts/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth import get_user_model
+
 
 # Create your tests here.
+class SignUpPageTests(TestCase):
+    def test_signup_page_loads(self):
+        response = self.client.get("/accounts/signup/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_signup_form_submission(self):
+        response = self.client.post(
+            reverse("signup"),
+            {
+                "username": "testuser",
+                "email": "testuser@gmail.com",
+                "password1": "testpassword",
+                "password2": "testpassword",
+            },
+        )
+
+        self.assertEqual(response.status_code, 302)
+
+    # Should redirect after successful signup
